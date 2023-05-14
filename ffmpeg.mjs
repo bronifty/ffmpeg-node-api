138b3a97b04698cff2e4c1bbb291e1bd17e2ce02
bronifty <bronifty@gmail.com>
2023-05-13 20:55:52 -0400

    .

--- ffmpeg.mjs
+++ ffmpeg.mjs
@@ -7,13 +7,7 @@ export async function processVideoToImage({
   inputFile,
   outputFile,
 }) {
-  // const inputFileName = `input-video`;
-  // const outputFileName = `output-image.png`;
   let outputData = null;
-
-  // console.log(
-  //   `in ffmpeg.mjs processVideoToImage; parsedCommand: "${parsedCommand}", inputFile: "${inputFile}", outputFile: "${outputFile}"`
-  // );
 
   const ffmpegInstance = createFFmpeg({ log: true });
   let ffmpegLoadingPromise = ffmpegInstance.load();
@@ -32,77 +26,17 @@ export async function processVideoToImage({
     inputFile,
     await fetchFile(path.join(process.cwd(), "./input.mov"))
   );
-  // ffmpeg.FS(
-  //   "writeFile",
-  //   "input.mov",
-  //   await fetchFile(path.join(process.cwd(), "./input.mov"))
-  // );
 
-  // await ffmpeg.run(parsedCommand);
-  // let tempCommandString = `"-ss","00:00:01.000","-i","${inputFile}","-frames:v","1","${outputFile}"`;
-  // console.log("tempCommandString", tempCommandString);
-
-  // await ffmpeg.run(tempCommandString);
-  // const commandArray = [
-  //   "-ss",
-  //   "00:00:01.000",
-  //   "-i",
-  //   inputFile,
-  //   "-frames:v",
-  //   "1",
-  //   outputFile,
-  // ];
   try {
     await ffmpeg.run(...parsedCommand);
   } catch (error) {
     console.log("error", error);
   }
 
-  // await ffmpeg.run(
-  //   "-ss",
-  //   "00:00:01.000",
-  //   "-i",
-  //   inputFile,
-  //   "-frames:v",
-  //   "1",
-  //   outputFile
-  // );
-  // this one works; order is important; let's make a test string var and pass it
-  // await ffmpeg.run(
-  //   "-ss",
-  //   "00:00:01.000",
-  //   "-i",
-  //   inputFile,
-  //   "-frames:v",
-  //   "1",
-  //   outputFile
-  // );
-  // await ffmpeg.run(
-  //   "-ss",
-  //   "00:00:01.000",
-  //   "-i",
-  //   inputFile,
-  //   "-frames:v",
-  //   "1",
-  //   outputFile
-  // );
-  // await ffmpeg.run(
-  //   "-ss",
-  //   "00:00:01.000",
-  //   "-i",
-  //   "input.mov",
-  //   "-frames:v",
-  //   "1",
-  //   "output.png"
-  // );
   outputData = ffmpeg.FS("readFile", outputFile);
-  // outputData = ffmpeg.FS("readFile", "output-image.png");
   ffmpeg.FS("unlink", inputFile);
   ffmpeg.FS("unlink", outputFile);
-  // ffmpeg.FS("unlink", "input.mov");
-  // ffmpeg.FS("unlink", "output-image.png");
 
-  // console.log("outputData", outputData);
   fs.writeFile(outputFile, outputData, "binary", (err) => {
     if (err) {
       console.error("Error writing the image file:", err);
